0e2b847dac1d97a97a31e15fa2ffc4fb3c7e2bcf
Thalles Maia <tmaiadev@gmail.com>
2026-01-07 10:06:27 -0300

    added welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { ComponentExample } from "@/components/component-example";
-
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to PodcastPlayer.app</h1>
+    </main>
+  );
 }
