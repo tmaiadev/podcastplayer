e219fa44edaaa516ac966a8b098efd90efa77580
Thalles Maia <tmaiadev@gmail.com>
2026-01-08 14:32:23 -0300

    fixed scrolling

--- components/podcast/category-section.tsx
+++ components/podcast/category-section.tsx
@@ -4,6 +4,7 @@ import type { Category, Podcast } from '@/lib/podcast-index';
 import type { SupportedLanguage } from '@/lib/i18n/constants';
 import { PodcastCard } from './podcast-card';
 import { Badge } from '@/components/ui/badge';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { getTranslations } from '@/lib/i18n/translations';
 
 interface CategorySectionProps {
@@ -25,11 +26,14 @@ export function CategorySection({ category, podcasts, language }: CategorySectio
         </Badge>
       </div>
 
-      <div className="overflow-x-auto flex gap-4 pb-2 px-1 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-        {displayPodcasts.map((podcast) => (
-          <PodcastCard key={podcast.id} podcast={podcast} language={language} />
-        ))}
-      </div>
+      <ScrollArea className="w-full">
+        <div className="flex gap-4 pb-2 px-1">
+          {displayPodcasts.map((podcast) => (
+            <PodcastCard key={podcast.id} podcast={podcast} language={language} />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </section>
   );
 }
